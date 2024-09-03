bdee75d8365df4e077a9d6c6e6f34718dd8eeba1
ekr0z <evan.kroz@gmail.com>
2024-09-02 21:14:56 -0400

    removed some non-essential comments, added some explanation comments for future amendments and work.

--- High Alchemy/high_alch.py
+++ High Alchemy/high_alch.py
@@ -5,27 +5,21 @@ from pynput import keyboard
 import random
 
 '''
-DONE: esc is now the global hotkey to exit the script
-TODO: Consistency issues with delay, pg.locateOnScreen() is not working? --> Not important
-TODO: Store cords in tuple, ex: self.top_left = (self.x_val-2, self.y_val-2) --> Not important
-TODO: Create a function in class HighAlch that finds valid points to click on to avoid ban --> In development
-TODO: Set mouse position cursor based on location of spell.png? --> unknown fix, package broken
-TODO: Find a way to move my cursor with ease in function from pg, and click
-NOTES: 1) Make sure cursor position is captured once at the start of the script
-       2) Create a seperate function for clicking? 
+NOTE: ESC is the hotkey to terminate the script, do not try and stop manually
+NOTE: Make sure cursor position is captured once at the start of the script
 '''
 
 
 class HighAlch(object):
     def __init__(self):
-        #capturing the initial mouse cursor position
-        #and evaluating corners for harder detection by anti-ban
-        #corners are the farthest the mouse should be allowed to move
+        #isnt consistent at all, rarely actually detects, something wrong with library?
         try:
             self.magic_location = pg.locateOnScreen(image="assets/spell.png")
         except pg.ImageNotFoundException:
             print("Image Not Found")
             print("Please Manually Place Your Cursor on High Alchemy Spell")
+
+        #initialiazes x and y values for cursor
         self.x_val = 0
         self.y_val = 0
 
@@ -37,10 +31,11 @@ class HighAlch(object):
         '''
 
     def clicker(self):
+        #while points are available to click on, and running = True, run the loop
         while self.x_val and self.y_val and running:
             #DANGER: DO NOT remove "and running" as tehre would be no means to test the script
 
-            #this is for 27 inch monitor, 2k
+            #works for 27 inch monitor, 2k
             local_x_val = self.x_val+random.randrange(-3, 3)
             local_y_val = self.y_val+random.randrange(-3, 3)
 
@@ -50,6 +45,8 @@ class HighAlch(object):
 
             magic_menu_delay = random.uniform(.2, .5)
             norm_inv_delay = random.uniform(1.9, 2.2)
+
+            #magic delay and moving functions for mouse
 
             pg.moveTo(x=local_x_val, y=local_y_val, duration=0.3, tween=pg.easeInQuad)
             print(f"x pos: {local_x_val}, y pos: {local_y_val}")
@@ -61,9 +58,9 @@ class HighAlch(object):
             print(f"Inventory Delay: {norm_inv_delay}")
             sleep(norm_inv_delay)
 
+    #styalized functions for cool-points
     def start(self):
         print("Program booting up...")
-        #loading bar for 10 seconds, not essential
         for _ in tqdm(range(5)):
             sleep(1)
         inp = input("Are you ready to begin program? Y/N")
@@ -72,14 +69,12 @@ class HighAlch(object):
             print("Program starting, be ready, have cursor on high alchemy spell in magic tab")
             for _ in tqdm(range(5)):
                 sleep(1)
-            self.x_val, self.y_val = pg.position() #--> function should be a static method, but this prevents
+            self.x_val, self.y_val = pg.position() #--> initializes mouse coordinates once user has mouse already on high alch spell
 
             print("Program has begun")
         else:
             print("Alright, Good Morning, Good Afternoon, Good Evening, and Good Night")
             print("Application Quit")
-
-    #create a main function for clicker and everything?
 
 
 running = True
